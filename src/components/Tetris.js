b02c3de39221a80099ad0f0dcc96a9d48512da0a
houmaiwakiri <mahomahoma888@gmail.com>
2022-08-19 23:10:32 +0900

    1:12:51

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,39 @@
-import React from "react";
-
-import { createStage } from "../gameHelpers";
-
+import React, {useState} from "react";
 //co,pomponents
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+//styled components
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
+//Custom Hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+
+
 
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+    
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+    console.log("re-render")
+
     return(
         <StyledTetrisWrapper>
             <StyledTetris>
-            <Stage stage={createStage()} />
+            <Stage stage={stage} />
             <aside>
+                {gameover ? (
+                    <Display gameOver={gameOver} text="Game Over" />
+                ) : (
                 <div>
                     <Display text="score" />
                     <Display text="Rows" />
                     <Display text="Level" />
                 </div>
+                )}
                 <StartButton />
             </aside>
             </StyledTetris>
