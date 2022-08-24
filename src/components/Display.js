94f6068dcb5666c1df8f9f527cdf7761f8fadaf2
houmaiwakiri <mahomahoma888@gmail.com>
2022-08-24 22:42:01 +0900

    完成。バグだらけ。内容も理解できてないけど、いい経験

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { StyledDisplay } from "./styles/styledDisplay";
+import React from 'react';
+import { StyledDisplay } from './styles/StyledDisplay';
 
-const Display = ({gameover, text}) => (
-    <StyledDisplay gameover={gameover}>{text}</StyledDisplay>
-)
+const Display = ({ gameOver, text }) => (
+  <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
+);
 
 export default Display;
